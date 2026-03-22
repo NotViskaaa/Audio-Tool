fcd1cc6f87b90ed5916f8d71edf329f608b54309
NotVskbi <bandiziolmichele@gmail.com>
2026-03-22 08:54:22 +0100

    config.js

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
-// Questo file contiene i tuoi dati privati.
-// NON caricare questo file su GitHub!
 const PRIVATE_CONFIG = {
     DISCORD_WEBHOOK_URL: "https://discord.com/api/webhooks/1485181801457647698/f_3HYfip8_BkLMpYwxUZ5-IUOCVaUrqI_VpB_0z1EVHBI2N9naccTckjpA4Hl3HEuiwj"
 };
